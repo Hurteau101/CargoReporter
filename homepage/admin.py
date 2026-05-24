a32fed850a6c8b06c4cd81adc3b96c5b54020aff
Devon <DevonHurteau@gmail.com>
2026-05-24 15:27:03 -0600

    Removed duplication from this location.

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,5 +1,1 @@
-from django.contrib import admin
 
-from homepage.models import DuplicateAWB
-
-admin.site.register([DuplicateAWB])
--- homepage/models.py
+++ homepage/models.py
@@ -15,15 +15,3 @@ class AWBBase(models.Model):
 
     class Meta:
         abstract = True
-
-
-
-class DuplicateAWB(models.Model):
-    awb_number = models.CharField(max_length=12, primary_key=True)
-
-    class Meta:
-        db_table = 'duplicate_awb'
-        verbose_name_plural = 'Duplicate AWBs'
-
-    def __str__(self):
-        return self.awb_number
