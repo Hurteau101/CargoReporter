9d9d3e998ee3b17c1a0eae5e2702920e2e113bd8
Devon <DevonHurteau@gmail.com>
2026-05-23 23:14:21 -0600

    Updated urls

--- under_30/urls.py
+++ under_30/urls.py
@@ -4,6 +4,6 @@ from under_30.views import Under30View, TransferAWBView, UpdateSentAWB
 
 urlpatterns = [
     path('', Under30View.as_view(), name='under_30'),
-    path('/transfer-awb', TransferAWBView.as_view(), name='transfer-awb'),
-    path('/update-sent-awb/<str:awb_number>', UpdateSentAWB.as_view(), name='update-sent-awb'),
+    path('transfer-awb', TransferAWBView.as_view(), name='transfer-awb'),
+    path('update-sent-awb/<str:awb_number>', UpdateSentAWB.as_view(), name='update-sent-awb'),
 ]
