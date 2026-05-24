c8d82d5ae9423e130e6b37fadd6474f48957f705
Devon <DevonHurteau@gmail.com>
2026-05-24 15:26:41 -0600

    WIP: Duplication app

---
+++ duplication/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from duplication.models import DuplicateAWB
+
+# Register your models here.
+admin.site.register(DuplicateAWB)
---
+++ duplication/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+
+class DuplicateAWB(models.Model):
+    awb_number = models.CharField(max_length=12, unique=True)
+    destination_iata = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'duplicate_awb'
+        verbose_name_plural = 'Duplicate AWBs'
+
+    def __str__(self):
+        return self.awb_number
