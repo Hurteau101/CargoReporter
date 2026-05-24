fd77d7a4d76ad202109c77c34bd04ffa5cacbe6d
Devon <DevonHurteau@gmail.com>
2026-05-24 14:50:50 -0600

    Added search field in admin panel.

--- under_30/admin.py
+++ under_30/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
 from under_30.models import AWBData
 
-admin.site.register(AWBData)
+@admin.register(AWBData)
+class AWBDataAdmin(admin.ModelAdmin):
+    search_fields = ['awb_number']
