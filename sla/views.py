5531c7e7b93ddf6888206777a3c585ad52c2af3c
Devon <DevonHurteau@gmail.com>
2026-05-24 16:15:15 -0600

    Completed sla

--- sla/views.py
+++ sla/views.py
@@ -1,7 +1,29 @@
+from collections import defaultdict
+from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
+from sla.models import SLA
 
 
 class SLAView(View):
     def get(self, request):
-        return render(request, 'sla.html')
+        sla = SLA.objects.all()
+        # sla_destination = sla.values_list('destination_iata', "hours_remaining").distinct()
+        sla_weights = sla.values("destination_iata").annotate(
+            total_weight=Sum("weight_on_hand")
+        ).order_by("-total_weight")
+
+        destinations = defaultdict(list)
+        for row in SLA.objects.values():
+            destinations[row['destination_iata']].append(row)
+
+        destinations = dict(destinations)
+
+        total_weight = sla.aggregate(Sum('weight_on_hand'))['weight_on_hand__sum']
+
+        return render(request, 'sla.html', context={
+            'sla': sla,
+            'sla_weights': sla_weights,
+            'destinations': destinations,
+            'total_weight': total_weight,
+        })
