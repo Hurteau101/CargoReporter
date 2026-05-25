c55178a5755cbe454560f30fd8ab1c815f3481bc
Devon <DevonHurteau@gmail.com>
2026-05-25 08:41:41 -0600

    Updated domain and ignore file

--- cargoreporter/settings.py
+++ cargoreporter/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1:8000", "127.0.0.1", "localhost", "cargoreporter.ca"]
+ALLOWED_HOSTS = ["127.0.0.1:8000", "127.0.0.1", "localhost", "cargo-reporter.ca"]
 
 
 # Application definition
